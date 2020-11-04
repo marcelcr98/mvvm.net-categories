589f7cf54e5ce4dc153a837e9c4508709c5725c4
marcelcr98 <mcuellarr98@gmail.com>
2020-11-04 00:05:17 -0500

    commit crud

---
+++ Semana 06/View/ListaCategoria.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Semana_06.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+namespace Semana_06.View
+{
+    /// <summary>
+    /// Lógica de interacción para ListaCategoria.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        ListaCategoriaViewModel viewModel;
+        public ListaCategoria()
+        {
+            InitializeComponent();
+            viewModel = new ListaCategoriaViewModel();
+            this.DataContext = viewModel;
+
+        }
+
+        public void Cargar()
+        {
+            BCategoria BCategoria = null;
+            try
+            {
+                BCategoria = new BCategoria();
+                dgvCategoria.ItemsSource = BCategoria.Listar(0);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ha ocurrido un error. Debe comunicarse con el Administrador");
+            }
+            finally
+            {
+                BCategoria = null;
+            }
+        }
+
+        private void dgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idCategoria;
+            var item = (Categoria)dgvCategoria.SelectedItem;
+            if (item == null) return;
+            idCategoria = Convert.ToInt32(item.IdCategoria);
+            View.ManCategoria window =  new View.ManCategoria();
+            window.ShowDialog();
+
+        }
+    }
+}
---
+++ Semana 06/ViewModel/ListaViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Entity;
+using System.Windows;
+
+namespace Semana_06.ViewModel
+{
+    public class ListaCategoriaViewModel : ViewModelBase
+    {
+        public ObservableCollection<Categoria> Categorias { get; set; }
+        public ICommand NuevoCommand { set; get; }
+        public ICommand ConsultarCommand { set; get; }
+        public ListaCategoriaViewModel()
+        {
+            Categorias = new Model.CategoriaModel().Categorias;
+            NuevoCommand = new RelayCommand<Window>(
+                param => Abrir()
+                );
+            ConsultarCommand = new RelayCommand<object>(
+                o => { Categorias = (new Model.CategoriaModel()).Categorias; });
+            void Abrir()
+            {
+                View.ManCategoria window = new View.ManCategoria();
+                window.ShowDialog();
+            }
+        }
+    }
+}
